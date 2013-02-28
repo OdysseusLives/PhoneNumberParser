461e4ff65260c60ff88f6e93734b7cb5a3e2cc6d
compwron <wordymusic@gmail.com>
2013-02-27 20:05:43 -0600

    cleaning out project

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,6 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        ConnectUtil connectUtil = new ConnectUtil();
-        AirportCodeHelper helper = new AirportCodeHelper();
-        List<String> airportCodes = helper.getAirportCodes();
-
-        for (String airportCode : airportCodes){
-            Airport airport = new Airport(connectUtil.getAirportData(airportCode));
-            System.out.println(airport.getSql());
-        }
+       
     }
 }
